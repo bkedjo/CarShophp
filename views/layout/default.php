7aee348bbc9e7798458692fb7b228ae3f36fbd8c
bkedjo <dpakson23@gmail.com>
2024-03-15 20:23:45 -0400

    ajout des fichiers principaux de views/.

---
+++ views/layout/default.php
@@ -0,0 +1,13 @@
+<header>
+    <?php
+    require_once ROOT."views/public/header.php";
+    ?>
+</header>
+<main>
+    <?php 
+    echo $contenu;
+    ?>
+</main>
+<footer>
+    <h3>je suis footer</h3>
+</footer>
